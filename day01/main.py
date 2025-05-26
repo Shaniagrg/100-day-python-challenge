09c7b8b175842725b0d140f5985350c1f314b9de
Shaniagrg <sglearner2022@gmail.com>
2025-05-26 13:03:51 -0700

    use input function and add space between variables

---
+++ day01/main.py
@@ -0,0 +1,8 @@
+print("Welcome to The Band Name Generator!")
+#print("What city did you grow up in?")
+#print("Toronto")
+city:str=input("What city did you grow up in?")
+#print("What is the name of your first pet?")
+#print("Gecko")
+pet:str=input("What is the name of your first pet?")
+print("Your band name is"+" " +city +" "+ pet) #Add space bewtween variable with " " 
