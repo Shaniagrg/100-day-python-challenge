d8f1d4f8e9b96ce26d9fd2ab92990955ef834758
Shaniagrg <sglearner2022@gmail.com>
2025-05-27 10:22:55 -0700

    ways to add space

--- day01/main.py
+++ day01/main.py
@@ -1,9 +1,8 @@
 print("Welcome to The Band Name Generator!")
-#print("What city did you grow up in?")
-#print("Toronto")
+
 city:str=input("What city did you grow up in? ")
-#print("What is the name of your first pet?")
-#print("Gecko")
+
 pet:str=input("What is the name of your first pet? ")
 #print("Your band name is"+" " +city +" "+ pet) #Add space bewtween variable with " " 
-print(f"Your band name is {city}{pet}")
+print(f"Your band name is {city} {pet}. ") #Enter space after you write city to create space between city and pet
+#Two ways to add space 
