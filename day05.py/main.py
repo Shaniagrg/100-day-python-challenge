6d15fc763c5e377ab00e0c716dd18c9c37484e57
Shaniagrg <sglearner2022@gmail.com>
2025-06-01 14:35:31 -0700

    Corrected the range in for loops to iterate properly based on user input and Moved variable declarations (a, b, c) inside their respective loops

--- day05.py/main.py
+++ day05.py/main.py
@@ -2,24 +2,24 @@ import random
 import string  
 password:list[str] = []
 final_password:str = ""
-a = random.choice(string.ascii_letters) 
-b = random.choice(string.digits)
-c = random.choice(string.punctuation)
 
 print("how many letters would you like in your password?")
 letters:int = int(input(""))
-for i in range(letters):
+for i in range(0,letters,+1):
+    a = random.choice(string.ascii_letters) 
     password.append(a)
 
 
 print("How many symbols would you like?")
 symbol: int = int(input())
-for ind in range(symbol):
+for ind in range(0,symbol,+1):
+    c = random.choice(string.punctuation)
     password.append(c)
 
 print("How many numbers would you like?")
 number:int = int(input())
-for inde in range(number):
+for inde in range(0,number+1):
+    b = random.choice(string.digits)
     password.append(b)
 
 for index in range(0,len(password),+1):
