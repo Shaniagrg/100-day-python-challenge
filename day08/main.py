e04a443f503217a521552bd82c5a18f73942db34
Shaniagrg <sglearner2022@gmail.com>
2025-06-09 20:22:00 -0700

    define data type for letters

--- day08/main.py
+++ day08/main.py
@@ -1,5 +1,5 @@
 
-letters:list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+letters:list[str] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 def funct_input() -> None:
     while True:
         print("Type 'encode' to encrypt, type 'decode' to decrypt:")
