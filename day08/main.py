1d3f419f93eddd9f1d534e7a14670bd83e84e4a4
Shaniagrg <sglearner2022@gmail.com>
2025-06-15 03:57:52 -0700

    Use function on all the segments

--- day08/main.py
+++ day08/main.py
@@ -1,6 +1,57 @@
+def convert_message_to_decode(letters_index:list[str],secret_message:str,secret:int) -> str:
+    '''
+    Decode the message by concating the string to form a word 
 
+    parameters:
+        - letters_index: list[str]
+        - secret_message:str
+        - secret:int
 
-def change_message(letters_index:list[str],convert_message_to:str,shift_number:str,message:str) -> str:
+    returns: str
+    '''
+    secret_message = secret_message + letters_index[secret] 
+    return secret_message
+
+def convert_message_to_encode(letters_index:list[str],secret_message:str,secret:int) -> str:
+    '''
+    Encode the message by concating the string to form a word 
+
+    parameters:
+        - letters_index: list[str]
+        - secret_message:str
+        - secret:int
+
+    returns: str
+    '''
+    secret_message = secret_message + letters_index[secret]
+    return secret_message
+
+def letter_position(letters_index:list[str],convert_message_to:str,shift_number:int,secret_message:str,current_letter:str) -> str:
+    '''
+    Position the letter serially and change based on shift number
+
+    parameters:
+        - letters_index: list[str]
+        - current_letter:str
+        - convert_message_to:str
+        - secret_message:str
+        - shift_number:int
+
+    returns: str
+    '''
+    for i in range(0,len(letters_index),1):
+        if current_letter == letters_index[i]:  
+            if convert_message_to == "encode":
+                secret:int = (i + int(shift_number)) % 26   
+                return convert_message_to_encode(letters_index,secret_message,secret)
+                
+            elif convert_message_to == "decode":
+                secret: int = (i - int(shift_number)) % 26  # minus - shift backward
+                return convert_message_to_decode(letters_index,secret_message,secret)
+                
+                
+
+def change_message(letters_index:list[str],convert_message_to:str,shift_number:str,message:str) -> None:
     '''
     first transfer the message and list of letters
     Then convert the message
@@ -16,20 +67,11 @@ def change_message(letters_index:list[str],convert_message_to:str,shift_number:s
     secret_message:str = ""
     for ind in range(0,len(message),1):
         current_letter:str = message[ind]
-        for i in range(0,len(letters_index),1):
-            if current_letter == letters_index[i]:  
-                if convert_message_to == "encode":
-                    secret:int = (i + int(shift_number)) % 26   
-                    secret_message = secret_message + letters_index[secret]
-                elif convert_message_to == "decode":
-                    secret: int = (i - int(shift_number)) % 26  # minus - shift backward
-                    secret_message = secret_message + letters_index[secret] 
-    return secret_message
-
+    print(f"Here's the result: '{letter_position(letters_index,convert_message_to,shift_number,secret_message,current_letter)}'.")
+    return None
     
-#print(f"Here's the result: {secret_message}.")
     
-def create_message(convert_message_to:str, letters_index:list[str]) -> str:
+def create_message(convert_message_to:str, letters_index:list[str]) -> None:
     '''
     Create message and the computer will encrypt/decrypt for you
     
@@ -43,8 +85,8 @@ def create_message(convert_message_to:str, letters_index:list[str]) -> str:
     message:str = input("")
     print("Type the shift number:")
     shift_number:int = input("")
-    #return secret_message 
-    return change_message(letters_index,convert_message_to,shift_number,message)
+    change_message(letters_index,convert_message_to,shift_number,message)
+    return None
 
 def play() -> None:
 
@@ -62,7 +104,7 @@ def play() -> None:
         print("Type 'encode' to encrypt, type 'decode' to decrypt:")
         type_word:str = input("")   
 
-        print(f"Here's the result: {create_message(type_word,letters)}")
+        create_message(convert_message_to=type_word,letters_index=letters)
 
         print("Do you want to run this program again?")
         yes_no:str = input("Type 'yes' or 'no': ")
