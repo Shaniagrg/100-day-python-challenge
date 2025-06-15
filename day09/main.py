e803f8158971743f24c6cebc0471fb7792f180ae
Shaniagrg <sglearner2022@gmail.com>
2025-06-14 18:15:35 -0700

    Add function and update winner selection logic to iterate through all bids

--- day09/main.py
+++ day09/main.py
@@ -1,32 +1,36 @@
-print("Welcome to the private bidding auction")
+def play(bidders_record):
 
-bidders:dict[str,float] = {}
-highest_bid:float = 0.0
-highest_bidder:dict[str,float] = {}
-
-while True:
-   
-
-    name:str = input("What is your name?: ")
-    bid_price:float = float(input("How much would you like to bid?: $"))
-    bidders[name] = bid_price
-    #print(bidders)
+    while True:
     
-    print("Are there any other bidders for this auction? Type 'yes' or 'no'.")
-    other_bidders:str = input()
-    
-    if other_bidders == "no":
-        for key,value in bidders.items():
-            if bidders[name] > highest_bid:
-                highest_bid = bidders[name]
-                if value == highest_bid:
-                    winner_name:str = key
-        print(f"The highest bidder is {winner_name} with a ${highest_bid}.")
-        print("Thank you for participation.")
-        break
-    elif other_bidders == "yes":
-        continue        
+        name:str = input("What is your name?: ")
+        bid_price:float = float(input("How much would you like to bid?: $"))
+        bidders_record[name] = bid_price
+        #print(bidders)
+        
+        print("Are there any other bidders for this auction? Type 'yes' or 'no'.")
+        other_bidders:str = input()
+        
+        if other_bidders == "no":
+            
+            highest_bid:float = 0.0
+            winner_name:str = ""
+            for key,value in bidders_record.items():
+                if value > highest_bid:
+                    highest_bid = value
+                    winner_name = key
+            print(f"The highest bidder is {winner_name} with a ${highest_bid}.")
+            print("Thank you for participation.")
+            break
+        elif other_bidders == "yes":
+            continue        
 
+def main() -> None:
+    print("Welcome to the private bidding auction")
+    bidders:dict[str,float] = {}
+    play(bidders)
+    
+if __name__ == '__main__': 
+    main()
 
 #yes then continue the loop and no then the end of the bid with thank you message 
 # map name:bid_price
