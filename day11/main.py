2d1fd78d667147a2a2c87f15bb256d5aa9c8edbf
Shaniagrg <sglearner2022@gmail.com>
2025-06-19 17:58:33 -0700

    Initial commit: First version of card game with deck and card selection

---
+++ day11/main.py
@@ -0,0 +1,32 @@
+import random
+
+jack:int = 10
+queen:int = 10
+king:int = 10
+ace:tuple[int,int] = (1,11)
+
+deck_map: dict[str, list[int]] = { 
+    'Hearts': [ace, 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , jack, queen, king],
+    'Diamonds': [ace, 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , jack, queen, king],
+    'Clubs': [ace, 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , jack, queen, king],
+    'Spades': [ace, 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , jack, queen, king]
+}
+
+computer_card:int = 0
+my_card:int = 0 
+for key,value in deck_map.items():
+    computer_card = random.choice(value)
+    if computer_card == ace:
+        computer_card = random.choice(ace)
+print(computer_card)
+
+#2 card each = random + random 
+#Ask continue or end
+    #if continue add 1 add a time 
+    #end then sum of card and compare
+#continue 
+#ask split or add
+  #if split then it becomes 2 individual 
+
+
+
