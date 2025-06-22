5b1a942f3445c0d049cdc713c0b4a44e79b1a2b2
Shaniagrg <sglearner2022@gmail.com>
2025-06-22 00:30:02 -0700

    Store user and computer cards as {card: [value1, value2]}

--- day11/main.py
+++ day11/main.py
@@ -1,16 +1,22 @@
 import random
 
-def play():
-    deck_map: dict[str, list[str]] = { 
-        'Hearts': ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"],
-        'Diamonds': ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"],
-        'Clubs': ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"],
-        'Spades': ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
-    }
+def play(deck_map:dict[str, list[str]],pairs_of_cards:list[list[str]]) -> None:
 
-    used_cards:list[list[str]] = []
-    computer_card:list[str] = []
-    my_card:list[str] = []
+    '''
+    - ramdomly choose 2 values out from the 4 ramdom kayes for both computer and user
+    - then create a variable to store used_cards to avoid repetition
+    - call funtion to continue/split/end game
+
+    parameters
+        - deck_map
+        - pairs_of_cards
+
+    return
+        -  None
+    '''
+    used_cards:list[list[str]]= []
+    computer_card:dict[str, list[str]] = {"cards": []}
+    my_card:dict[str, list[str]] = {"cards": []}
 
     for i in range (0,2,1):
 
@@ -22,7 +28,7 @@ def play():
                 continue
             else:
                 used_cards.append(random_key_value)
-                my_card.append(random_value)
+                my_card["cards"].append(random_value)
                 break
             
         while True:
@@ -33,11 +39,50 @@ def play():
                 continue
             else:
                 used_cards.append(random_key_value)
-                computer_card.append(random_value)
+                computer_card["cards"].append(random_value)
                 break
+
+    print(f"Your cards: {my_card}")
+    print(f"The computer's first card: {computer_card['cards'][0]}")
+    print(used_cards)
+    print(my_card)
+    print(computer_card)
+
+
 def main() -> None:
+    '''
+    Create a map for all the cards 
+    Also create pairs for split
+
+    parameters:
+        - None
+    
+    return:
+        None
+    '''
     print("Lets play blackjack")
-    print (play())
+    deck: dict[str, list[str]] = { 
+        'Hearts': ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"],
+        'Diamonds': ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"],
+        'Clubs': ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"],
+        'Spades': ["ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"]
+    }
+    pairs:list[list[str]] = [
+        ["ace", "ace"],
+        ["jack", "jack"],
+        ["2", "2"],
+        ["3", "3"],
+        ["4", "4"],
+        ["5", "5"],
+        ["6", "6"],
+        ["7", "7"],
+        ["8", "8"],
+        ["9", "9"],
+        ["10", "10"],
+        ["queen", "queen"],
+        ["king", "king"]
+    ]
+    print (play(deck,pairs))
     
 
 if __name__ == '__main__': 
