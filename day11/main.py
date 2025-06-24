b94338988469dc6b476504641f2d1d64bf10d6aa
Shaniagrg <sglearner2022@gmail.com>
2025-06-24 06:19:39 -0700

    create function for getting random value and append

--- day11/main.py
+++ day11/main.py
@@ -16,22 +16,74 @@ while True:
             print("You Win!!!")
         elif sum_my_card < sum_computer_card:
             print("You lost =(")
-            
+    
+    def ace_conversion(convert:list[str]) -> str:
+            '''
+            till here the list[str] its still string
 
-    def append_value(deck_map:dict[str, list[str]],used_cards:list[list[str]], my_card:dict[str, list[int]]):
+            - sum the total value from convert[list[str]] and store in total_value type int
+            - use total_value to determine the value of ace
+            - store the ace value in ace_value_determine as string 
+
+            parameter:
+                - convert:list[str]
+
+            return:
+                - string
+            
+            '''
+            total_value:int = 0
+            ace_value_determine:str = ""
+
+            for i in range(0,len(convert),1):
+                if convert[i] == "ace":
+                    continue
+                elif convert[i] == "jack" or convert[i] == "queen" or convert[i] == "king":
+                    total_value = total_value + 10
+                else:
+                    total_value = total_value + int(convert[i])
+
+
+            if total_value <= 10:
+                ace_value_determine = "11"
+                return ace_value_determine
+            elif total_value > 11:
+                ace_value_determine = "1"
+                return ace_value_determine
+
+    def convert_value(convert:list[str]) -> list[int]:
+        '''
+        - convert the jack,queen and king to "10" and ace to "11" or "1" in strings 
+        - then type converion from list[string] to list[int]
+
+        parameters:
+            - convert:list[str]
+
+        return:
+            - list[int]
+        '''
+
+        for i in range(0,len(convert),1):
+            if convert[i] == "jack" or convert[i] == "queen" or convert[i] == "king":
+                convert[i] = "10"
+            elif convert[i] == "ace":
+                convert[i] = ace_conversion(convert)
+
+        for ind in range(0,len(convert),1):
+            convert[ind] = int(convert[ind])
+        return convert
+    
+    def get_random_value(deck_map:dict[str, list[str]],used_cards:list[list[str]]) -> str:
 
         '''
-        - Choose ramdom key then value
-        - append used_cards to avoid repetition
-        - appned random_value to the my_card and convert to int
+        - ramdomly choose 2 values out from the 4 ramdom keys for both computer and user
 
         parameter:
             - deck_map:dict[str, list[str]]
             - used_cards:list[list[str]]
-            - my_card:dict[str, list[int]]
 
-        return:
-            -  dict[str, list[int]]
+        return
+            - string
         '''
         while True:
             random_key:str = random.choice(list(deck_map.keys()))
@@ -41,9 +93,25 @@ while True:
                 continue
             else:
                 used_cards.append(random_key_value)
-                my_card["hand1"].append(random_value)
-                convert_value(my_card["hand1"])
-                return my_card
+                return random_value
+
+    def append_value(deck_map:dict[str, list[str]],used_cards:list[list[str]], adding_card:dict[str, list[str]]) -> None:
+
+        '''
+        - call function get_random_value to get random value
+        - append random_value to the my_card and convert to int
+
+        parameter:
+            - deck_map:dict[str, list[str]]
+            - used_cards:list[list[str]]
+            - adding_card:dict[str, list[str]]
+
+        return:
+            -  None
+        '''
+        append_random_value:str = get_random_value(deck_map,used_cards)
+        adding_card["hand1"].append(append_random_value)
+        convert_value(adding_card["hand1"])
 
     def split_check(deck_map:dict[str, list[str]],used_cards:list[list[str]],my_card:dict[str, list[int]], computer_card:list[int]) -> dict[str, list[int]]:
 
@@ -90,63 +158,9 @@ while True:
             sum_cards_value = sum_cards_value + sum[i]
         return sum_cards_value
 
-    def ace_conversion(convert:list[str]) -> str:
-        '''
-        till here the list[str] its still string
-
-        - sum the total value from convert[list[str]] and store in total_value type int
-        - use total_value to determine the value of ace
-        - store the ace value in ace_value_determine as string 
-
-        parameter:
-            - convert:list[str]
-
-        return:
-            - string
-        
-        '''
-        total_value:int = 0
-        ace_value_determine:str = ""
-
-        for i in range(0,len(convert),1):
-            if convert[i] == "ace":
-                continue
-            elif convert[i] == "jack" or convert[i] == "queen" or convert[i] == "king":
-                total_value = total_value + 10
-            else:
-                total_value = total_value + int(convert[i])
-
-
-        if total_value <= 10:
-            ace_value_determine = "11"
-            return ace_value_determine
-        elif total_value > 11:
-            ace_value_determine = "1"
-            return ace_value_determine
-
-    def convert_value(convert:list[str]) -> list[int]:
-        '''
-        - convert the jack,queen and king to "10" and ace to "11" or "1" in strings 
-        - then type converion from list[string] to list[int]
-
-        parameters:
-            - convert:list[str]
-
-        return:
-            - list[int]
-        '''
-
-        for i in range(0,len(convert),1):
-            if convert[i] == "jack" or convert[i] == "queen" or convert[i] == "king":
-                convert[i] = "10"
-            elif convert[i] == "ace":
-                convert[i] = ace_conversion(convert)
-
-        for ind in range(0,len(convert),1):
-            convert[ind] = int(convert[ind])
-        return convert
 
     def check_21(user:list[str],dealer:list[str]) -> str:
+
         '''
         - call function convert the value for jack,queen,ace and king
         - call function to sum the card to check if its over 21 or not
@@ -173,14 +187,12 @@ while True:
     def play(deck_map:dict[str, list[str]]) -> None:
 
         '''
-        - ramdomly choose 2 values out from the 4 ramdom kayes for both computer and user
-        - then create a variable to store used_cards to avoid repetition
+        - loop function append_value to get 2 values for computer/user
+        - create a variable to store used_cards to avoid repetition
         - call funtion to continue/split/end game
 
         parameters
             - deck_map
-            - pairs_of_cards
-
         return
             -  None
         '''
@@ -193,28 +205,8 @@ while True:
 
 
         for i in range (0,2,1):
-
-            while True:
-                random_key:str = random.choice(list(deck_map.keys()))
-                random_value:str = random.choice(deck_map[random_key])
-                random_key_value:list[str] = [random_key,random_value]
-                if random_key_value in used_cards:
-                    continue
-                else:
-                    used_cards.append(random_key_value)
-                    my_card["hand1"].append(random_value)
-                    break
-                
-            while True:
-                random_key:str = random.choice(list(deck_map.keys()))
-                random_value:str = random.choice(deck_map[random_key])
-                random_key_value:list[str] = [random_key,random_value]
-                if random_key_value in used_cards:
-                    continue
-                else:
-                    used_cards.append(random_key_value)
-                    computer_card["hand1"].append(random_value)
-                    break
+            append_value(deck_map,used_cards,adding_card=my_card)
+            append_value(deck_map,used_cards,adding_card=computer_card)
 
         print(f"Your cards: {my_card}")
         print(f"The computer's first card: {computer_card['hand1'][0]}")
@@ -235,11 +227,8 @@ while True:
         my_card = split_check(deck_map,used_cards,my_card,computer_card["hand1"])
         print(my_card)
 
-        continue_game:str = input("Type 'y' to get another card, type 'n' to pass: ")
+        #continue_game:str = input("Type 'y' to get another card, type 'n' to pass: ")
         
-
-
-
     def main() -> None:
         '''
         Create a map for all the cards 
