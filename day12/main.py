f9e442c69027a2d0ee5e8890c45869374a2e85f7
Shaniagrg <sglearner2022@gmail.com>
2025-07-02 23:19:50 -0700

    Implement difficulty selection for guessing game, allowing user to choose between 'easy' and 'hard' levels

--- day12/main.py
+++ day12/main.py
@@ -23,7 +23,48 @@ def pointer_for_guess(user_number_guess:int ,answer:int, p1:int, p2:int) -> list
     guess_range:list[int] = [p1,p2]
     return guess_range
 
-def user_guessing(computer_number: int) -> None:
+def user_guessing_easy(computer_number: int) -> None:
+
+    '''
+    user get 5 tries 
+    append th gussed letter to avoid repeatition 
+    
+    parameter:
+        - guess_number: int
+
+    return:
+        - None
+    '''
+    tries:int = 5 
+    guessed_number: list[int] = []
+    pointer_beginning:int = 1
+    pointer_end:int = 21
+
+    while True:
+        user_guess:int = int(input("Take a guess: "))
+        if user_guess in guessed_number:
+            print(f"Already used number. Try another.")
+            continue
+        elif user_guess not in guessed_number:
+            if user_guess == computer_number:
+                print(f"Correct! The answer was {computer_number} . Thanks for completing that!")
+                break 
+            else:
+                tries = tries - 1
+                print(f"You have {tries} guesses left for the number that I'm thinking of.")
+                user_guess_range:int = pointer_for_guess(user_number_guess = user_guess  , answer=computer_number, p1=pointer_beginning, p2=pointer_end)
+                pointer_beginning = user_guess_range[0]
+                pointer_end = user_guess_range[1]
+                
+                if tries == 0:
+                    print(f"Game over. The word was '{computer_number}.'")
+                    break 
+                else:
+                    guessed_number.append(user_guess)
+    return            
+
+
+def user_guessing_hard(computer_number: int) -> None:
 
     '''
     user get 5 tries 
@@ -86,10 +127,18 @@ def play() -> None:
         print("Welcome to Guess The number")
 
         print("I'm thinking of a number between 1 to 100, try to guess it.")
-        game_level:str = input("Choose a difficulty. Type 'easy' or 'hard': ")
-       
-        user_guessing(guess_number)
-             
+        while True:
+
+            game_level:str = input("Choose a difficulty. Type 'easy' or 'hard': ")
+            if game_level == 'hard':
+                user_guessing_hard(guess_number)
+                break
+            elif game_level == 'easy':
+                user_guessing_easy(guess_number)
+                break
+            else:
+                print("Type the correct word")
+                continue
         #print("too high or too low")
     
         continue_game:str = input("Do you want to play again? Type 'y' if yes and 'n' to quit:  ")
