4a46f4fb7a13b76c2985e725adbed74d92bec4a6
Shaniagrg <sglearner2022@gmail.com>
2025-07-06 23:16:49 -0700

    Created a function people that randomly selects a celebrity from a predefined dictionary of celebrities

---
+++ day14/README.md
@@ -0,0 +1,11 @@
+<h3>Day 14 - Higher Lower Game Project</h3>
+
+Concepts Practised
+<ol>
+    <li>Using Dictionary Data</li>
+    <li>Using Procedures</li>
+    <li>Using Conditional Statements</li>
+</ol>
+
+
+
---
+++ day14/main.py
@@ -0,0 +1,37 @@
+import random
+def people() -> list[str]:
+    
+    celebrities:dict[str,list[str]] ={"100": [ "Neymar, a Footballer, from Brasil"], 
+                                        "600": ["Ariana Grande, a Musician and actress, from United States"], 
+                                        "20": ["Justin Bieber, a Musician, from Canada"],
+                                        "55": ["Chiritiano, a footballer, from Spain"],
+                                        "86": ["Jeff Bezoz. founder of Amazon, from United States"],
+                                        "11": ["Kancha, founder of AI , from Dubai"],
+                                        "2": ["Mark, founder of Facebook, from United States"]}
+    for key,value in celebrities.items():
+        random_key:str = random.choice(list(celebrities.keys())) 
+        random_value:str = random.choice(celebrities[random_key])
+        random_key_value:list[str] = [random_key,random_value]
+        return random_key_value
+
+while True:
+    #correct add\n
+    random_people_A:list[str] = people()
+    compare_a = print(f"Compare A: {random_people_A[1]} ")
+
+    random_people_B:list[str] = people()
+    compare_b = print(f"Compare B: {random_people_B[1]}")
+    user_input:str = input("Who do you think has more followers? Type'a', or 'b': ")
+
+
+    #incorrect you score is 
+    continue_game:str = input("Do you want to play again? Type 'y' or 'n': ")
+    if continue_game == 'y':
+        continue
+    elif continue_game == 'n':
+        print("Thanks for playing")
+        break
+    else:
+        print("type the correct word")
+        continue
+
