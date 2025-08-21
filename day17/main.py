f4bdb2b4557a68e97aa33489fbed1967adf42290
Shaniagrg <sglearner2022@gmail.com>
2025-08-21 07:06:34 -0700

    Created a Quiz class to manage true/false questions and user interactions

--- day17/main.py
+++ day17/main.py
@@ -1,35 +1,42 @@
 import random
+class Quiz:
 
-total_questions:list[list[str]] = [
-    ["A slug's blood is green.", "True"],
-    ["The loudest animal is the African Elephant." , "False"],
-    ["Approximately one quarter of human bones are in the feet.", "True"],
-    ["The total surface area of a human lungs is the size of a football pitch." , "True"],
-    ["In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home "
-             "to eat." , "True"],
-    ["In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral." , "False"],
-    ["It is illegal to pee in the Ocean in Portugal." , "True"],
-    ["You can lead a cow down stairs but not up stairs." , "False"],
-    ["Google was originally called 'Backrub'." , "True"],
-    ["Buzz Aldrin's mother's maiden name was 'Moon'." , "True"],
-    ["No piece of square dry paper can be folded in half more than 7 times." , "False"],
-    ["A few ounces of chocolate can to kill a small dog.", "True"]
-]
+    total_questions:list[list[str]] = [
+        ["A slug's blood is green.", "True"],
+        ["The loudest animal is the African Elephant." , "False"],
+        ["Approximately one quarter of human bones are in the feet.", "True"],
+        ["The total surface area of a human lungs is the size of a football pitch." , "True"],
+        ["In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home "
+                "to eat." , "True"],
+        ["In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral." , "False"],
+        ["It is illegal to pee in the Ocean in Portugal." , "True"],
+        ["You can lead a cow down stairs but not up stairs." , "False"],
+        ["Google was originally called 'Backrub'." , "True"],
+        ["Buzz Aldrin's mother's maiden name was 'Moon'." , "True"],
+        ["No piece of square dry paper can be folded in half more than 7 times." , "False"],
+        ["A few ounces of chocolate can to kill a small dog.", "True"]
+    ]
 
-points:int = 0
-Q:int = 0
+    points:int = 0
+    Q:int = 0
+    used_question:list[list[str]] = []
 
-used_question:list[list[str]] = []
-question:list[str] = random.choice(total_questions)
+    @classmethod
+    def start_quiz(cls):
+        while True:
+            question:list[str] = random.choice(cls.total_questions)
+            print(question[0])
+            cls.used_question.append(question)
+            answer:str = input("True/False: ")
+            if answer == question[1]:
+                cls.Q = cls.Q + 1
+                cls.points = cls.points + 1
+                print(f"Your current score is {cls.points}/{cls.Q}")
+            elif cls.Q == 12:
+                print(f"Your current score is {cls.points}/{cls.Q}")
+                break
+            else:
+                cls.Q = cls.Q + 1
+                print(f"Your current score is {cls.points}/{cls.Q}")
 
-print(question[0])
-
-used_question.append(question)
-answer:str = input("True/False: ")
-if answer == question[1]:
-    Q = Q + 1
-    points = points + 1
-    print(f"Your current score is {points}/{Q}")
-else:
-    Q = Q + 1
-    print(f"Your current score is {points}/{Q}")
+Quiz.start_quiz()
