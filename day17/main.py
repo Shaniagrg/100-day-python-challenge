72e19931e6a43a0c2f7a654880d374ec14ca0469
Shaniagrg <sglearner2022@gmail.com>
2025-08-24 17:05:56 -0700

    Refactor quiz to allow dynamic question addition and prevent duplicates

--- day17/main.py
+++ day17/main.py
@@ -10,23 +10,41 @@ class Quiz:
     def start_quiz(self):
         points:int = 0
         Q:int = 0
-        used_question:list[list[str]] = []
+        used_question:set[str] = set()
         while True:
             question:list[str] = random.choice(self.total_questions)
-            print(question[0])
-            used_question.append(question)
-            answer:str = input("True/False: ")
-            if answer == question[1]:
-                Q = Q + 1
-                points = points + 1
-                print(f"Your current score is {points}/{Q}")
-            elif Q == 12:
-                print(f"Your current score is {points}/{Q}")
+            if len(used_question) == len(self.total_questions):
+                print("Thanks for completing that")
+                print(f"Your final score is:{points}/{Q}")
+                exit_quiz:str = input("Process finish with exit code 0: ")
                 break
+            elif question[0] in used_question:
+                continue
             else:
-                Q = Q + 1
-                print(f"Your current score is {points}/{Q}")
+                while True:
+                    print(question[0])
+                    used_question.add(question[0])
+                    answer:str = input("True/False: ")
+                    if answer == "True" or answer == "False":
+                        if answer == question[1]:
+                            Q = Q + 1
+                            points = points + 1
+                            print(f"You got it right!!!")
+                            print(f"The correct answer was: {question[1]}")
+                            print(f"Your current score is {points}/{Q}")
+                        else:
+                            Q = Q + 1
+                            print("That's incorrect")
+                            print(f"The correct answer was: {question[1]}")
+                            print(f"Your current score is {points}/{Q}")
+                        break
+                    else:
+                        print("Type the correct word.")
+                        continue
+            
 
+                
+                
 question:list[list[str]] = [["A slug's blood is green.", "True"],
         ["The loudest animal is the African Elephant." , "False"],
         ["Approximately one quarter of human bones are in the feet.", "True"],
