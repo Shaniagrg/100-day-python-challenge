05f44bc0eee79ceeae018402392da8c953668619
Shaniagrg <sglearner2022@gmail.com>
2025-08-26 01:06:47 -0700

    Improve Quiz structure by utilizing functions for better organization

--- day17/main.py
+++ day17/main.py
@@ -2,8 +2,6 @@ import random
 class Quiz:
 
     all_question:dict[str,str] = {}
-    #total_questions:list[list[str]] = []
-    
     def __init__(self):
         self.all_question = {"A slug's blood is green.":"True",
         "The loudest animal is the African Elephant." :"False",
@@ -21,40 +19,49 @@ class Quiz:
         
 
     def start_quiz(self):
-        points:int = 0
-        Q:int = 0
+        user_total_points:list[int] = [0,0]
         used_question:set[str] = set()
         while True:
             question:list[str] = random.choice(list(self.all_question.items()))
             if len(used_question) == len(self.all_question):
-                print("Thanks for completing that")
-                print(f"Your final score is:{points}/{Q}")
-                exit_quiz:str = input("Process finish with exit code 0: ")
+                self.end_game(user_total_points)
                 break
             elif question[0] in used_question:
                 continue
             else:
-                while True:
+                self.check_answer(question,user_total_points,used_question)
+            
+    def end_game(self,points:list[int]):
+        print("Thanks for completing that")
+        print(f"Your final score is:{points[0]}/{points[1]}")
+        exit_quiz:str = input("Process finish with exit code 0: ")
+
+    def check_answer(self,question:list[str],user_total_points:list[int], used_question:set[str]):
+        points:int = user_total_points[0]
+        Q_count:int = user_total_points[1]
+        while True:
                     print(question[0])
                     used_question.add(question[0])
                     answer:str = input("True/False: ")
                     if answer == "True" or answer == "False":
                         if answer == question[1]:
-                            Q = Q + 1
+                            Q_count = Q_count + 1
                             points = points + 1
                             print(f"You got it right!!!")
                             print(f"The correct answer was: {question[1]}")
-                            print(f"Your current score is {points}/{Q}")
+                            print(f"Your current score is {points}/{Q_count}")
                         else:
-                            Q = Q + 1
+                            Q_count = Q_count + 1
                             print("That's incorrect")
                             print(f"The correct answer was: {question[1]}")
-                            print(f"Your current score is {points}/{Q}")
+                            print(f"Your current score is {points}/{Q_count}")
                         break
                     else:
                         print("Type the correct word.")
                         continue
-            
+        user_total_points[0] = points
+        user_total_points[1] = Q_count
+        #return(user_total_points)
 
 Quiz_sam:Quiz = Quiz()
 Quiz_sam.start_quiz()
