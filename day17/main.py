762e4fcd751c73b6fa450902643ea785c995e600
Shaniagrg <sglearner2022@gmail.com>
2025-08-25 23:13:38 -0700

    Modify question structure to a dictionary for better organization

--- day17/main.py
+++ day17/main.py
@@ -12,7 +12,7 @@ class Quiz:
         Q:int = 0
         used_question:set[str] = set()
         while True:
-            question:list[str] = random.choice(self.total_questions)
+            question:list[str] = random.choice(list(self.total_questions.items()))
             if len(used_question) == len(self.total_questions):
                 print("Thanks for completing that")
                 print(f"Your final score is:{points}/{Q}")
@@ -45,19 +45,20 @@ class Quiz:
 
                 
                 
-question:list[list[str]] = [["A slug's blood is green.", "True"],
-        ["The loudest animal is the African Elephant." , "False"],
-        ["Approximately one quarter of human bones are in the feet.", "True"],
-        ["The total surface area of a human lungs is the size of a football pitch." , "True"],
-        ["In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home "
-                "to eat." , "True"],
-        ["In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral." , "False"],
-        ["It is illegal to pee in the Ocean in Portugal." , "True"],
-        ["You can lead a cow down stairs but not up stairs." , "False"],
-        ["Google was originally called 'Backrub'." , "True"],
-        ["Buzz Aldrin's mother's maiden name was 'Moon'." , "True"],
-        ["No piece of square dry paper can be folded in half more than 7 times." , "False"],
-        ["A few ounces of chocolate can to kill a small dog.", "True"]]
+question:dict[str,str] = {"A slug's blood is green.":"True",
+        "The loudest animal is the African Elephant." :"False",
+        "Approximately one quarter of human bones are in the feet." : "True",
+        "The total surface area of a human lungs is the size of a football pitch." : "True",
+        "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home "
+                "to eat." : "True",
+        "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral." : "False",
+        "It is illegal to pee in the Ocean in Portugal." : "True",
+        "You can lead a cow down stairs but not up stairs." : "False",
+        "Google was originally called 'Backrub'." : "True",
+        "Buzz Aldrin's mother's maiden name was 'Moon'." : "True",
+        "No piece of square dry paper can be folded in half more than 7 times." : "False",
+        "A few ounces of chocolate can to kill a small dog.": "True"}
     
+
 Quiz_sam:Quiz = Quiz(question)
 Quiz_sam.start_quiz()
