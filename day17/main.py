a3a9baa39add541539dad7b523c1bfd5f786cf5a
Shaniagrg <sglearner2022@gmail.com>
2025-08-27 06:43:52 -0700

    Refactor Quiz class to use instance variables for points, Q_count, and used_question; enable independent quizzes for multiple players

--- day17/main.py
+++ day17/main.py
@@ -1,9 +1,13 @@
 import random
 class Quiz:
 
-    all_question:dict[str,str] = {}
     def __init__(self):
-        self.all_question = {"A slug's blood is green.":"True",
+        self.all_question = self.question()
+        self.points:int = 0
+        self.Q_count:int = 0
+        self.used_question:set[str] = set()
+    def question(self) -> dict[str,str]:
+        question:dict[str,str] = {"A slug's blood is green.":"True",
         "The loudest animal is the African Elephant." :"False",
         "Approximately one quarter of human bones are in the feet." : "True",
         "The total surface area of a human lungs is the size of a football pitch." : "True",
@@ -16,52 +20,48 @@ class Quiz:
         "Buzz Aldrin's mother's maiden name was 'Moon'." : "True",
         "No piece of square dry paper can be folded in half more than 7 times." : "False",
         "A few ounces of chocolate can to kill a small dog.": "True"}
-        
+        return question
 
     def start_quiz(self):
-        user_total_points:list[int] = [0,0]
-        used_question:set[str] = set()
         while True:
             question:list[str] = random.choice(list(self.all_question.items()))
-            if len(used_question) == len(self.all_question):
-                self.end_game(user_total_points)
+            if len(self.used_question) == len(self.all_question):
+                self.end_game()
                 break
-            elif question[0] in used_question:
+            elif question[0] in self.used_question:
                 continue
             else:
-                self.check_answer(question,user_total_points,used_question)
+                self.check_answer(question)
             
-    def end_game(self,points:list[int]):
+    def end_game(self):
         print("Thanks for completing that")
-        print(f"Your final score is:{points[0]}/{points[1]}")
+        print(f"Your final score is:{self.points}/{self.Q_count}")
         exit_quiz:str = input("Process finish with exit code 0: ")
 
-    def check_answer(self,question:list[str],user_total_points:list[int], used_question:set[str]):
-        points:int = user_total_points[0]
-        Q_count:int = user_total_points[1]
+    def check_answer(self,question:list[str]):
         while True:
                     print(question[0])
-                    used_question.add(question[0])
+                    self.used_question.add(question[0])
                     answer:str = input("True/False: ")
                     if answer == "True" or answer == "False":
                         if answer == question[1]:
-                            Q_count = Q_count + 1
-                            points = points + 1
+                            self.Q_count = self.Q_count + 1
+                            self.points = self.points + 1
                             print(f"You got it right!!!")
                             print(f"The correct answer was: {question[1]}")
-                            print(f"Your current score is {points}/{Q_count}")
+                            print(f"Your current score is {self.points}/{self.Q_count}")
                         else:
-                            Q_count = Q_count + 1
+                            self.Q_count = self.Q_count + 1
                             print("That's incorrect")
                             print(f"The correct answer was: {question[1]}")
-                            print(f"Your current score is {points}/{Q_count}")
+                            print(f"Your current score is {self.points}/{self.Q_count}")
                         break
                     else:
                         print("Type the correct word.")
                         continue
-        user_total_points[0] = points
-        user_total_points[1] = Q_count
-        #return(user_total_points)
 
 Quiz_sam:Quiz = Quiz()
 Quiz_sam.start_quiz()
+
+Quiz_tina:Quiz = Quiz()
+Quiz_tina.start_quiz()
