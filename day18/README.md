70a4512824f8f16c30d20d555ec55b2ad83e7a69
Shaniagrg <sglearner2022@gmail.com>
2025-08-29 07:07:48 -0700

    use trutle to create octagon

---
+++ day18/README.md
@@ -0,0 +1,11 @@
+<h3>Day 18 - Turtle & the Graphical User Interface (GUI)</h3>
+
+<p>Concepts Practised<p>
+
+<ol>
+    <li>Understanding Turtle Graphics and How to use the Documentation</li>
+    <li>Importing Modules, Installing Packages, and Working with Aliases</li>
+    <li>Python Tuples and How to Generate Random RGB Colours</li>
+</ol>
+
+
---
+++ day18/main.py
@@ -0,0 +1,23 @@
+from turtle import *
+'''
+pensize(4)  # Make the lines thicker than normal.
+
+forward(200)  # Move the turtle forward 200 steps.
+left(90)  # Turn the turtle left by 90 degrees.
+
+# Move forward and turn three more times:
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+
+done()  # Without this, the Turtle window may immediately close before you can see the picture.s
+'''
+pensize(4)
+
+for i in range(8):
+    forward(100)
+    left(45)  
+done()
