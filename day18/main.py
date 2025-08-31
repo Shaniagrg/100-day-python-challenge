c487b2b1843d534d2bed3043e068c93a6a7171b5
Shaniagrg <sglearner2022@gmail.com>
2025-08-31 03:37:36 -0700

    use loop and add space using forward after each shape

--- day18/main.py
+++ day18/main.py
@@ -2,14 +2,16 @@ import turtle
 import random
 colors = ['red', 'orange', 'yellow', 'blue', 'green', 'purple','pink']
 
-turtle.pensize(1)
-turtle.pencolor('white')
-turtle.fillcolor(random.choice(colors))
-turtle.speed('normal')
-turtle.begin_fill() 
+for i in range(0,10,1):
+    turtle.pensize(1)
+    turtle.pencolor('white')
+    turtle.fillcolor(random.choice(colors))
+    turtle.speed('fastest')
+    turtle.begin_fill() 
 
-for i in range(8):
-    turtle.forward(100)
-    turtle.left(45)
-turtle.end_fill()
-turtle.done()
+    for i in range(8):
+        turtle.forward(20)
+        turtle.left(45)
+    turtle.end_fill()
+    turtle.forward(70)
+    turtle.done()
