2618752082e48cd7085c14c430bf8a68e2e031d9
Shaniagrg <sglearner2022@gmail.com>
2025-09-01 18:55:10 -0700

    Added functionality to move octagons up and repeat drawing with left and right turns

--- day18/main.py
+++ day18/main.py
@@ -1,6 +1,15 @@
 import turtle
 import random
 colors = ['red', 'orange', 'yellow', 'blue', 'green', 'purple','pink']
+#seatheading: 180 going back and 360 going front
+
+def left_turn():
+  turtle.setheading(180)
+  turtle.forward(10)
+  
+def right_turn():
+  turtle.setheading(360)
+  turtle.forward(10)
 
 def octagon():
     turtle.pensize(1)
@@ -14,7 +23,12 @@ def octagon():
         turtle.left(45)
     turtle.end_fill()
     turtle.forward(40)
+for i in range (0,4,1):
+    for i in range(0,10,1):
+        octagon()
+        left_turn()
 
-for i in range(0,10,1):
-    octagon()
+    for i in range(0,10,1):
+        octagon()  
+        right_turn() 
 turtle.done()
