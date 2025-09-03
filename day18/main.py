f0114b6c54bfb322ded3d0ffddf1a193a7e08f0c
Shaniagrg <sglearner2022@gmail.com>
2025-09-02 23:46:56 -0700

    adjusted forward and direction for the octagon

--- day18/main.py
+++ day18/main.py
@@ -1,34 +1,48 @@
 import turtle
 import random
+t = turtle.Turtle()
+t.speed(5) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
 colors = ['red', 'orange', 'yellow', 'blue', 'green', 'purple','pink']
-#seatheading: 180 going back and 360 going front
+#seatheading: 180 going back and 0 going front
 
 def left_turn():
-  turtle.setheading(180)
-  turtle.forward(10)
+  t.setheading(180)
   
 def right_turn():
-  turtle.setheading(360)
-  turtle.forward(10)
-
+  t.setheading(0)
+  
+def up_turn():
+	t.setheading(90)
+def down_turn():
+	t.setheading(270)
+  
 def octagon():
-    turtle.pensize(1)
-    turtle.pencolor('white')
-    turtle.fillcolor(random.choice(colors))
-    turtle.speed('fastest')
-    turtle.begin_fill() 
+  t.pensize(1)
+  t.pencolor('white')
+  t.fillcolor(random.choice(colors))
+  t.speed('fastest')
+  t.begin_fill() 
 
-    for i in range(8):
-        turtle.forward(10)
-        turtle.left(45)
-    turtle.end_fill()
-    turtle.forward(40)
-for i in range (0,4,1):
-    for i in range(0,10,1):
-        octagon()
-        left_turn()
-
-    for i in range(0,10,1):
-        octagon()  
-        right_turn() 
-turtle.done()
+  for i in range(8):
+    t.forward(10)
+    t.left(45)
+  t.end_fill()
+  t.forward(40)
+for ind in range(4): 
+    for i in range (4):
+      left_turn()
+      octagon()
+      #left_turn
+      t.forward(30)
+    up_turn()
+    t.forward(30)
+    right_turn()
+    for inde in range(4):
+      #t.forward(30)
+      octagon()
+      t.forward(30)
+    up_turn()
+    t.forward(30)
+    left_turn()
+    
+t.done()
