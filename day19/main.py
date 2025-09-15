d0e06db6ddacaeed5492740661dd19c209a3b28a
Shaniagrg <sglearner2022@gmail.com>
2025-09-14 22:34:18 -0700

    Implement rectangular arena while maintaining circular player representation; update Players class for proper initialization

--- day19/main.py
+++ day19/main.py
@@ -3,7 +3,7 @@ import random
 t = turtle.Turtle()
 t.speed(0) # 1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
 colors = ['red', 'orange', 'blue', 'green', 'purple','pink']
-class Field:
+class Arena:
     f = 0
     l = 0
     def __init__(self, forward=250, left=90):
@@ -11,23 +11,31 @@ class Field:
         self.l = left 
 
     def size_field(self):
-        for i in range(4):
+        for i in range(4): #make square
             t.forward(self.f)
             t.left(self.l)
 
 class Players:
+    radius = 0
+    speed = 0
+    colors = []
+    def __init__(self,r=12, s=0):
+        self.radius = r
+        self.speed = s
+        self.colors = ['red', 'orange', 'yellow', 'blue', 'green', 'purple','pink']
     def player(self):
-        t.pencolor(random.choice(colors))
-        #t.penup(40)
-        for i in range(30):
-            t.forward(1 + i)
-            t.left(91)
+        t.speed(self.speed)
+        t.pencolor('white')
+        t.fillcolor(random.choice(self.colors))
+        t.begin_fill() 
+        t.circle(self.radius)
+        t.end_fill() 
             
 class Start:
-    Field.f1 = None
+    Arena.f1 = None
     Players.p = None
     def __init__(self):
-        self.f1 = Field()   # create a field
+        self.f1 = Arena()   # create a field
         self.p = Players() 
          
     def field_1(self):
