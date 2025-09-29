635ecc0109a50f59ac70b3dc672a10c0df802b26
Shaniagrg <sglearner2022@gmail.com>
2025-09-28 22:21:59 -0700

    create class to draw snake

--- day20/main.py
+++ day20/main.py
@@ -22,7 +22,35 @@ class Arena:
         self.screen.setup(self.width, self.height)
         self.screen.bgcolor(self.bg_color)
 
+class Snake:
+    def __init__(self, length, width, color, speed, x, y):
+        self.snake = turtle.Turtle()
+        self.length = length
+        self.width = width
+        self.color = color
+        self.speed = speed
+        self.position = self.snake_position(x,y)
+    
+    def snake_position(self,x,y):
+        self.snake.up()
+        self.snake.setposition(x,y)
+        self.snake.color(self.color)
+        self.draw_snake()
+        
+    def draw_snake(self):
+        self.snake.speed(self.speed)
+        self.snake.begin_fill()  # color fill
+        for index in range(2):
+            self.snake.forward(self.length)  
+            self.snake.right(90)  
+            self.snake.forward(self.width)  
+            self.snake.right(90) 
+        self.snake.end_fill()
+        
 class Game:
+    @classmethod
+    def create_snake(cls):
+        cls.snake = Snake(length=40, width = 10, color = "green", speed = 0, x = 0, y = 0)
     @classmethod
     def create_arena(cls):
         cls.arena = Arena(width = 500, height = 500, bg_color = "lavender")
@@ -31,6 +59,7 @@ class Game:
     @classmethod
     def start(cls):
         cls.create_arena()
+        cls.create_snake()
         
         
 Game.start()
