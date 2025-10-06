21e45315bc45999b3a36f970890902ec8efcb66a
Shaniagrg <sglearner2022@gmail.com>
2025-10-05 23:50:08 -0700

    use turtle shape for snake creation

--- day20/main.py
+++ day20/main.py
@@ -23,8 +23,9 @@ class Arena:
         self.screen.bgcolor(self.bg_color)
 
 class Snake:
-    def __init__(self, length, width, color, speed, x, y):
+    def __init__(self, shape, length, width, color, speed, x, y):
         self.snake = turtle.Turtle()
+        self.shape = shape
         self.length = length
         self.width = width
         self.color = color
@@ -33,25 +34,21 @@ class Snake:
     
     def snake_position(self,x,y):
         self.snake.up()
+        self.snake.shape(self.shape)
         self.snake.setposition(x,y)
         self.snake.color(self.color)
-        self.draw_snake()
         
-    def draw_snake(self):
-        self.snake.speed(self.speed)
-        self.snake.begin_fill()  # color fill
-        for index in range(2):
-            self.snake.forward(self.length)  
-            self.snake.right(90)  
-            self.snake.forward(self.width)  
-            self.snake.right(90) 
-        self.snake.end_fill()
+    '''
     
-    '''def move_snake(self):
-        self.x = self.x + 1 #move right until width 500
-        self.x = self.x - 1 #move left until width 500
-        self.y = self.y + 1 #move up until lenght 500
-        self.y = self.y - 1 #move down until length 500'''
+    def move_snake(self):
+        if direction == "right":
+            self.x = self.x + 1 #move right until width 500
+        if direction == "left":
+            self.x = self.x - 1 #move left until width 500
+        if direction == "up":
+            self.y = self.y + 1 #move up until lenght 500
+        if direction == "down":
+            self.y = self.y - 1 #move down until length 500'''
    
 class Food:
     def __init__(self, shape, color, food_x, food_y):
@@ -71,7 +68,7 @@ class Game:
         cls.food = Food(shape = "circle",color = "red", food_x = -50, food_y = 30)
     @classmethod
     def create_snake(cls):
-        cls.snake = Snake(length=40, width = 10, color = "green", speed = 0, x = 0, y = 0)
+        cls.snake = Snake(shape = "square", length=40, width = 10, color = "green", speed = 0, x = 0, y = 0)
     @classmethod
     def create_arena(cls):
         cls.arena = Arena(width = 500, height = 500, bg_color = "lavender")
